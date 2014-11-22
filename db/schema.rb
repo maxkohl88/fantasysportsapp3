0a5e6e04be387f48558b02785f4327f67e67062a
Max Kohl <maxkohl810@gmail.com>
2014-11-21 21:19:47 -0500

    add owner column to daily_stat_line table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141122021309) do
+ActiveRecord::Schema.define(version: 20141122021507) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20141122021309) do
     t.integer "turnovers"
     t.integer "points"
     t.integer "fantasy_points"
+    t.string  "owner"
   end
 
   create_table "league_stats_tables", force: true do |t|
